bced0c3e03545490845ee539cfc66a593f24ff71
Portkas.D <646124200@qq.com>
2021-07-12 11:30:17 +0800

    组件间通信注释

--- src/views/Communication/components/ShowEvent/ShowEvent.jsx
+++ src/views/Communication/components/ShowEvent/ShowEvent.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Button } from 'antd'
 import { Checkbox } from 'antd';
 
@@ -9,16 +9,22 @@ const CheckboxGroup = Checkbox.Group;
 export default function ShowEvent(props) {
 
     const plainOptions = [{ label: '', value: props.item.item }]
-
+    /* 选择框状态改变 */
     const onChange = (list) => {
         props.handleCheckItem(list, props.item.item)
     }
+    /* 删除某一项 */
     const handleDel = () => {
         props.delSomeOne(props.item.item)
     }
     return (
         <div className='ShowEvent'>
-            <CheckboxGroup options={plainOptions} value={props.value} onChange={onChange} style={{ marginLeft: '20px' }} />
+            <CheckboxGroup
+                options={plainOptions}
+                value={props.value}
+                onChange={onChange}
+                style={{ marginLeft: '20px' }}
+            />
             <p>{props.item.item}</p>
             <Button type="primary" danger onClick={handleDel}>删除</Button>
         </div>
